18f35b6758c0fd3fd1985eacafcea83f61020648
MateuszWawrzyczek <mat.wawrzyczek@gmail.com>
2025-03-31 11:31:54 +0200

    generowanie GTFS-ow

--- brygady/obj/Debug/net8.0/brygady.AssemblyInfo.cs
+++ brygady/obj/Debug/net8.0/brygady.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("brygady")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d9ad55db51aec0b722e1ff3ce369f413750152a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d88f963bcc0715e374bf570433f4a5c0bcc3a6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("brygady")]
 [assembly: System.Reflection.AssemblyTitleAttribute("brygady")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
